de9b325c1f6ad8839da92ea99848869a72b81fb5
Andreas Thor <dr.andreas.thor@googlemail.com>
2018-06-27 15:00:51 +0200

    points in onyx

--- includes/imp/IMP_Item_ONYX.php
+++ includes/imp/IMP_Item_ONYX.php
@@ -20,60 +20,83 @@ class IMP_Item_ONYX extends IMP_Item {
 	 */
 	public function parseItemsFromImportFile(array $file): array {
 		
-		// check if zip file
-		if (substr($file['name'], - 4) != ".zip")
-			throw new Exception("Error! File is not a zip file");
+		// extract uploaded zip file into main dir
+		$mainDir = $this->extractZipFileToDirectory ($file['tmp_name']);
+		$directories = [$mainDir];	// list of directories containing extracted zip files
 		
-		// extract zip ==> $dir = directory of extracted zip
-		$zip = new ZipArchive();
-		$res = $zip->open($file['tmp_name']);
-		if (! ($res === TRUE))
-			throw new Exception("Error when opening zip file! ErrorCode=" . $res);
-		$dir = sys_get_temp_dir() . "/eal" . microtime(TRUE);
-		$zip->extractTo($dir);
-		$zip->close();
+		// check if the zip files contains other zip files; if yes: extract them
+		foreach (scandir($mainDir) as $filename) {
+			if (substr ($filename, -4) == '.zip') {
+				$directories[] = $this->extractZipFileToDirectory ($mainDir . '/' . $filename);
+			}
+		}
 		
-		/* load main document (imsmanifest.xml) */
-		$file_imsmanifest = file_get_contents("{$dir}/imsmanifest.xml");
-		if ($file_imsmanifest == false)
-			throw new Exception("Could not find imsmanifest.xml file");
-		
-		/* get the list of items */
-		$doc_imsmanifest = new DOMDocument();
-		$doc_imsmanifest->loadXML($file_imsmanifest);
-		$itemsFromManifest = $this->parseManifest($doc_imsmanifest); // [Identifier => XML-Filename]
-		
-		$items = [];
 		$current_item_id = 0;
-		foreach ($itemsFromManifest as $identifier => $href) {
+		$items = [];
+		
+		foreach ($directories as $dir) {
+		
+			/* try load main document (imsmanifest.xml) */
+			if (!file_exists ("{$dir}/imsmanifest.xml")) {
+				continue;	// this is ok, if the main directory did contain other zips
+			}
+
+			/* get the list of items */
+			$doc_imsmanifest = new DOMDocument();
+			$doc_imsmanifest->loadXML(file_get_contents("{$dir}/imsmanifest.xml"));
+			$itemsFromManifest = $this->parseManifest($doc_imsmanifest); // [Identifier => XML-Filename]
 			
-			/* load item's xml file */
-			$file_item = file_get_contents("{$dir}/{$href}");
-			if ($file_item == false)
-				throw new Exception("Could not find item file " . $href);
+			foreach ($itemsFromManifest as $identifier => $href) {
+				
+				/* load item's xml file */
+				$file_item = file_get_contents("{$dir}/{$href}");
+				if ($file_item == false) {
+					throw new Exception("Could not find item file " . $href);
+				}
+					
+				/* parse item data */
+				$doc_item = new DOMDocument();
+				$doc_item->loadXML($file_item);
+				$item = $this->parseItem($doc_item, $dir, --$current_item_id);
+				$items[$item->getId()] = $item;
+			}
 			
-			$current_item_id --;
-			/* parse item data */
-			$doc_item = new DOMDocument();
-			$doc_item->loadXML($file_item);
-			$item = $this->parseItem($doc_item, $dir, $current_item_id);
-			$items[$item->getId()] = $item;
 		}
 		
 		return $items;
 	}
 
+	
+	/**
+	 * @param string $filename	zip file name
+	 * @throws Exception
+	 * @return string	directory where the zipfile has been extracted to
+	 */
+	private function extractZipFileToDirectory (string $filename): string {
+		
+		$zip = new ZipArchive();
+		$res = $zip->open($filename);
+		if (! ($res === TRUE)) {
+			throw new Exception("Error when opening zip file! ErrorCode=" . $res);
+		}
+		$dir = sys_get_temp_dir() . "/eal" . microtime(TRUE);	// ensures a unique directory name
+		$zip->extractTo($dir);
+		$zip->close();
+		return $dir;
+	}
+	
+	
 
 
 	/**
 	 * returns map of all questions; identifier --> xml-file name
 	 * identifier is the internal id for referencing questions
-	 *
-	 * @param DOMDocument $doc
+	 * @param DOMDocument $dom manifest XML file 
+	 * @return array [identifier => xml file name]
 	 */
-	private function parseManifest(DOMDocument $doc): array {
+	private function parseManifest(DOMDocument $dom): array {
 		$res = array();
-		foreach ($doc->documentElement->getElementsByTagName('resources')[0]->getElementsByTagName('resource') as $resource) {
+		foreach ($dom->documentElement->getElementsByTagName('resources')[0]->getElementsByTagName('resource') as $resource) {
 			$type = $resource->getAttribute("type");
 			if ($type === 'imsqti_item_xmlv2p1') { // TODO: further item types
 				$res[$resource->getAttribute("identifier")] = $resource->getAttribute("href");
@@ -86,31 +109,30 @@ class IMP_Item_ONYX extends IMP_Item {
 
 	/**
 	 *
-	 * @param DOMDocument $doc
+	 * @param DOMDocument $dom
 	 * @return
 	 */
-	private function parseItem(DOMDocument $doc, string $dir, int $default_item_id): EAL_Item {
+	private function parseItem(DOMDocument $dom, string $dir, int $default_item_id): EAL_Item {
 		
-		/* get item type */
-		$itembody = $doc->documentElement->getElementsByTagName('itemBody')[0];
+		/* get item content */
+		$itembody = $dom->documentElement->getElementsByTagName('itemBody')->item(0);
 		if (! ($itembody instanceof DOMElement)) {
 			throw new Exception('Could no found itembody.');
 		}
-		$choiceInteraction = $itembody->getElementsByTagName('choiceInteraction')[0];
-		if (! ($choiceInteraction instanceof DOMElement)) {
-			throw new Exception('Could no found choiceInteraction.');
+
+		/* determine desription and question = innerHTML of itembody except <choiceInteraction> */
+		$descques = '';
+		foreach ($itembody->childNodes as $child) {
+			if ($child instanceof DOMElement) {
+				if ($child->tagName == 'choiceInteraction') {
+					continue;
+				}
+			}
+			$descques .= $itembody->ownerDocument->saveHTML($child);
 		}
 		
-		/* generate item (SC or MS) */
-		$maxChoices = intval($choiceInteraction->getAttribute('maxChoices'));
-		$item = ($maxChoices == 1) ? new EAL_ItemSC($default_item_id) : new EAL_ItemMC($default_item_id);
-		
-		/* determine desription and question */
-		$itembody->removeChild($choiceInteraction);
-		$descques = $this->DOMinnerHTML($itembody);
-		
-		
-		$descques = preg_replace_callback(				// replace image references
+		/*  replace image references */
+		$descques = preg_replace_callback(				
 			'|(<img[^>]+)src=["\']([^"]*)["\']|',
 			function ($match) use ($dir) {
 				
@@ -120,18 +142,17 @@ class IMP_Item_ONYX extends IMP_Item {
 				}
 				
 				/* generate unique filename for img */
-				$count=0;
-				$path = wp_upload_dir()["path"];
-				$filename = urlencode($match[2]);
-				while (file_exists($path . "/" . $filename . "_" . $count)) {
-					$count++;
-				}
+				$filename = urlencode($match[2]);	// encode to make sure we have a vaild name without special characters
+				$count = 0;
+				do {
+					$toFile = wp_upload_dir()['path'] . '/' . (++$count) . '_' . $filename;
+				} while (file_exists($toFile));
 				
-				$from = $dir . '/' . urldecode($match[2]);
-				$to = $path . "/" . $filename . "_" . $count;
-				copy($from, $to);
+				/* copy file: match[2]'s format is usually "media/xyz.jp" where "/" is encoded as %2F */
+				copy($dir . '/' . urldecode($match[2]), $toFile);
 				
-				return $match[1] . "src='" . wp_upload_dir()["url"] . "/" . $filename . "_" . $count . "'";
+				/* return adjusted HTML code */
+				return $match[1] . "src='" . wp_upload_dir()['url']  . '/' . $count . '_' . urlencode ($filename) . "'";
 			},
 			$descques
 			);
@@ -139,45 +160,116 @@ class IMP_Item_ONYX extends IMP_Item {
 		$split = explode(EXP_Item::DESCRIPTION_QUESTION_SEPARATOR, $descques, 2); // Description and Question are separated by horizontal line; description is optional
 		
 		$object = [];
-		$object['post_title'] = $doc->documentElement->getAttribute('title');
+		$object['post_title'] = $dom->documentElement->getAttribute('title');
 		$object['item_description'] = (count($split) > 1) ? $split[0] : '';
 		$object['item_question'] = $split[count($split) - 1];
 		
-		/* get the answers */
-		$correctAnswerIdentifiers = [];
-		foreach ($doc->documentElement->getElementsByTagName('responseDeclaration')[0]->getElementsByTagName('correctResponse')[0]->getElementsByTagName('value') as $value) {
-			if ($value instanceof DOMNode) {
-				$correctAnswerIdentifiers[] = $value->nodeValue;
+		
+		/* determine item type */
+		$responseDeclaration = $dom->documentElement->getElementsByTagName('responseDeclaration')->item(0);
+		if ($responseDeclaration instanceof DOMElement) {
+			
+			if ($responseDeclaration->getAttribute('cardinality') == 'single') {
+				return $this->parseItemSC($dom, new EAL_ItemSC($default_item_id), $object);
 			}
+			
+			if ($responseDeclaration->getAttribute('cardinality') == 'multiple') {
+				return $this->parseItemMC($dom, new EAL_ItemMC($default_item_id), $object);
+			}
+			
+		}
+		throw new Exception ('Could not identify item type!');
+		
+		
+		$choiceInteraction = $itembody->getElementsByTagName('choiceInteraction')->item(0);
+		if (! ($choiceInteraction instanceof DOMElement)) {
+			throw new Exception('Could no found choiceInteraction.');
 		}
 		
+	}
+
+	private function parseItemSC(DOMDocument $dom, EAL_ItemSC $item, array $object): EAL_ItemSC {
+
+		$xpath = new DOMXPath($dom);
+		$rootNamespace = $dom->lookupNamespaceUri($dom->namespaceURI);
+		$xpath->registerNamespace('x', $rootNamespace);
+		
+		/* get the correct answer */
+		$correctAnswerIdentifier = $xpath->evaluate('/x:assessmentItem/x:responseDeclaration/x:correctResponse/x:value')[0]->nodeValue;
+
+		/* get the max score */
+		$maxscore = $xpath->evaluate('/x:assessmentItem/x:outcomeDeclaration[@identifier="MAXSCORE"]/x:defaultValue/x:value')[0]->nodeValue;
+		
+		/* get all answer options */
 		$object['answer'] = [];
-		if ($item instanceof EAL_ItemSC) {
-			$object['points'] = [];
+		$object['points'] = [];
+		$choiceInteraction = $xpath->evaluate('/x:assessmentItem/x:itemBody/x:choiceInteraction')[0];
+		if ($choiceInteraction instanceof DOMElement) {
 			foreach ($choiceInteraction->getElementsByTagName('simpleChoice') as $simpleChoice) {
 				if ($simpleChoice instanceof DOMElement) {
 					$object['answer'][] = $this->DOMinnerHTML($simpleChoice);
-					$object['points'][] = in_array($simpleChoice->getAttribute('identifier'), $correctAnswerIdentifiers) ? 1 : 0;
-				}
-			}
-		}
-		if ($item instanceof EAL_ItemMC) {
-			$object['positive'] = [];
-			$object['negative'] = [];
-			foreach ($choiceInteraction->getElementsByTagName('simpleChoice') as $simpleChoice) {
-				if ($simpleChoice instanceof DOMElement) {
-					$object['answer'][] = $this->DOMinnerHTML($simpleChoice);
-					$object['positive'][] = in_array($simpleChoice->getAttribute('identifier'), $correctAnswerIdentifiers) ? 1 : 0;
-					$object['negative'][] = in_array($simpleChoice->getAttribute('identifier'), $correctAnswerIdentifiers) ? 0 : 1;
+					$object['points'][] = $simpleChoice->getAttribute('identifier') == $correctAnswerIdentifier ? $maxscore : 0;
 				}
 			}
 		}
 		
 		$item->initFromArray($object, '', '');
 		return $item;
+		
 	}
+	
+	private function parseItemMC(DOMDocument $dom, EAL_ItemMC $item, array $object): EAL_ItemMC {
+		
+		$xpath = new DOMXPath($dom);
+		$rootNamespace = $dom->lookupNamespaceUri($dom->namespaceURI);
+		$xpath->registerNamespace('x', $rootNamespace);
+		
+		/* get the correct answers */
+		$correctAnswerIdentifiers = [];
+		foreach ($dom->documentElement->getElementsByTagName('responseDeclaration')[0]->getElementsByTagName('correctResponse')[0]->getElementsByTagName('value') as $value) {
+			if ($value instanceof DOMNode) {
+				$correctAnswerIdentifiers[] = $value->nodeValue;
+			}
+		}
 
+		/* get the points mappings for all answers (if available) */
+		$mapEntry = $xpath->evaluate('/x:assessmentItem/x:responseDeclaration/x:mapping/x:mapEntry');
+		$answersToPoints = [];
+		foreach ($mapEntry as $map) {
+			if ($map instanceof DOMElement) {
+				$answersToPoints[$map->getAttribute('mapKey')] = intval ($map->getAttribute('mappedValue'));
+			}
+		}
+		
+		
+		$object['answer'] = [];
+		$object['positive'] = [];
+		$object['negative'] = [];
+		$choiceInteraction = $xpath->evaluate('/x:assessmentItem/x:itemBody/x:choiceInteraction')[0];
+		if ($choiceInteraction instanceof DOMElement) {
+			foreach ($choiceInteraction->getElementsByTagName('simpleChoice') as $simpleChoice) {
+				if ($simpleChoice instanceof DOMElement) {
+					$object['answer'][] = $this->DOMinnerHTML($simpleChoice);
 
+					$identifier = $simpleChoice->getAttribute('identifier');
+					if (in_array($identifier, $correctAnswerIdentifiers)) {
+						$object['positive'][] = array_key_exists ($identifier, $answersToPoints) ? $answersToPoints[$identifier] : 1;
+						$object['negative'][] = 0;
+					} else {
+						$object['positive'][] = array_key_exists ($identifier, $answersToPoints) ? $answersToPoints[$identifier] : -1;
+						$object['negative'][] = 0;
+					}
+				}
+			}
+		}
+		
+		$item->initFromArray($object, '', '');
+		$item->maxnumber = intval($choiceInteraction->getAttribute('maxChoices'));
+		return $item;
+	}
+	
+
+	
 
 	private function DOMinnerHTML(DOMNode $element) {
 		$innerHTML = "";
