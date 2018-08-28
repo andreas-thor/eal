2403c3e7ef53233c60eafa5f954be03a7dfe916e
Andreas Thor <dr.andreas.thor@googlemail.com>
2018-08-28 10:37:48 +0200

    ilias export for free text

--- includes/exp/EXP_Item_Ilias.php
+++ includes/exp/EXP_Item_Ilias.php
@@ -13,8 +13,7 @@ class EXP_Item_Ilias extends EXP_Item {
 	}
 	
 	protected function generateExportFile (array $itemids) {
-	
-		
+
 		$zip = new ZipArchive();
 		$zip->open($this->getDownloadFullname(), ZipArchive::CREATE);
 		$zip->addFromString("{$this->getDownloadFileName()}/{$this->getDownloadFileName()}.xml", $this->createQPL($itemids)->saveXML());
@@ -67,6 +66,32 @@ class EXP_Item_Ilias extends EXP_Item {
 	}
 	
 	
+	private function getQuestionType (EAL_Item $item): string {
+		if ($item instanceof EAL_ItemSC) return 'SINGLE CHOICE QUESTION';
+		if ($item instanceof EAL_ItemMC) return 'MULTIPLE CHOICE QUESTION';
+		if ($item instanceof EAL_ItemFT) return 'TEXT QUESTION';
+	}
+	
+	private function getQuestionIdent (EAL_Item $item): string {
+		if ($item instanceof EAL_ItemSC) return 'MCSR';
+		if ($item instanceof EAL_ItemMC) return 'MCMR';
+		if ($item instanceof EAL_ItemFT) return 'TEXT';
+	}
+	
+	
+	private function getResponseElement (DOMDocument $dom, EAL_Item $item): DOMElement {
+		if ($item instanceof EAL_ItemSC) return $this->createResponseElementForChoice($dom, $item, 'Single');
+		if ($item instanceof EAL_ItemMC) return $this->createResponseElementForChoice($dom, $item, 'Multiple');
+		if ($item instanceof EAL_ItemFT) return $this->createResponseElementForText($dom, $item);
+	}
+	
+	private function getResprocessingElement (DOMDocument $dom, EAL_Item $item): DOMElement {
+		if ($item instanceof EAL_ItemSC) return $this->createResprocessingElementForChoice($dom, $item);
+		if ($item instanceof EAL_ItemMC) return $this->createResprocessingElementForChoice($dom, $item);
+		if ($item instanceof EAL_ItemFT) return $this->createResprocessingElementForText($dom, $item);
+	}
+	
+	
 	private function createQTI ($itemids) {
 	
 		$dom = DOMDocument::loadXML (
@@ -82,23 +107,7 @@ class EXP_Item_Ilias extends EXP_Item {
 			if ($post == null) continue;	// item (post) does not exist
 			
 			$item = DB_Item::loadFromDB($item_id, $post->post_type);
-			assert(($item instanceof EAL_ItemSC) || ($item instanceof EAL_ItemMC));
-			
-			if ($item instanceof EAL_ItemSC) {
-				$item_data = array (
-						"questiontype" => "SINGLE CHOICE QUESTION",
-						"ident" => "MCSR",
-						"rcardinality" => "Single"
-				);
-			}
-			
-			if ($item instanceof EAL_ItemMC) {
-				$item_data = array (
-						"questiontype" => "MULTIPLE CHOICE QUESTION",
-						"ident" => "MCMR",
-						"rcardinality" => "Multiple"
-				);
-			}
+			assert(($item instanceof EAL_ItemSC) || ($item instanceof EAL_ItemMC) || ($item instanceof EAL_ItemFT));
 			
 			$xml_IT = $dom->createElement("item");
 			$xml_IT->setAttribute("ident", "il_0_qst_{$item_id}");
@@ -112,7 +121,7 @@ class EXP_Item_Ilias extends EXP_Item {
 			$xml_QM = $dom->createElement("qtimetadata");
 			$meta = array (
 					"ILIAS_VERSION" => "5.0.8 2015-11-24",
-					"QUESTIONTYPE" => $item_data["questiontype"],
+					"QUESTIONTYPE" => $this->getQuestionType($item),
 					"AUTHOR" => get_the_author_meta ('login', get_post_field( 'post_author', $item->getId() )),
 					"additional_cont_edit_mode" => "default",
 					"externalId" => "il_0_qst_{$item_id}",
@@ -134,78 +143,14 @@ class EXP_Item_Ilias extends EXP_Item {
 			$xml_PR = $dom->createElement("presentation");
 			$xml_PR->setAttribute("label", $item->getTitle());
 			$xml_FL = $dom->createElement("flow");
-
 			$xml_FL->appendChild ($this->createMaterialElement($dom, "text/html", wpautop($item->getDescription()) . EXP_Item::DESCRIPTION_QUESTION_SEPARATOR . wpautop($item->getQuestion())));
-				
-			$xml_RL = $dom->createElement("response_lid");
-			$xml_RL->setAttribute("ident", $item_data["ident"]);
-			$xml_RL->setAttribute("rcardinality", $item_data["rcardinality"]);
-	
-			$xml_RC = $dom->createElement("render_choice");
-			$xml_RC->setAttribute("shuffle", "Yes");
-			
-
-			if ($item instanceof EAL_ItemMC) {
-				$xml_RC->setAttribute("minnumber", $item->minnumber);
-				$xml_RC->setAttribute("maxnumber", $item->maxnumber);
-			}			
-			
-			
-			for ($index=0; $index<$item->getNumberOfAnswers(); $index++) {
-				$xml_LAB = $dom->createElement("response_label");
-				$xml_LAB->setAttribute("ident", $index);
-				$xml_LAB->appendChild ($this->createMaterialElement($dom, "text/html", $item->getAnswer ($index)));
-				$xml_RC->appendChild ($xml_LAB);
-			}
-				
-			$xml_RL->appendChild ($xml_RC);
-			$xml_FL->appendChild ($xml_RL);
+			$xml_FL->appendChild ($this->getResponseElement($dom, $item));
 			$xml_PR->appendChild ($xml_FL);
 			$xml_IT->appendChild ($xml_PR);
-				
-			$xml_RP = $dom->createElement("resprocessing");
-				
-			$xml_OC = $dom->createElement("outcomes");
-			$xml_DV = $dom->createElement("decvar");
-			$xml_OC->appendChild ($xml_DV);
-			$xml_RP->appendChild ($xml_OC);
-	
-			for ($index=0; $index<$item->getNumberOfAnswers(); $index++) {
-				foreach (array (1, 0) as $checked) {
-						
-					$xml_RC = $dom->createElement("respcondition");
-					$xml_RC->setAttribute ("continue", "Yes");
-						
-					$xml_CV = $dom->createElement("conditionvar");
-					$xml_NO = $dom->createElement("not");
-					$xml_VE = $dom->createElement("varequal", $index);
-					$xml_VE->setAttribute ("respident", $item_data["ident"]);
-						
-					if ($checked==1) {
-						$xml_CV->appendChild ($xml_VE);
-					} else {
-						$xml_NO->appendChild ($xml_VE);
-						$xml_CV->appendChild ($xml_NO);
-					}
-					$xml_RC->appendChild ($xml_CV);
-						
-					
-					if ($item instanceof EAL_ItemSC) {
-						$xml_SV = $dom->createElement("setvar", ($checked==1) ? $item->getPointsChecked($index) : 0);
-					}
-					if ($item instanceof EAL_ItemMC) {
-						$xml_SV = $dom->createElement("setvar", ($checked==1) ? $item->getPointsPos($index) : $item->getPointsNeg($index));
-					}
-					
-					$xml_SV->setAttribute ("action", "Add");
-					$xml_RC->appendChild ($xml_SV);
-					$xml_RP->appendChild ($xml_RC);
-						
-				}
-	
-			}
-				
-			$xml_IT->appendChild ($xml_RP);
+
+			
+			/* Resprocessing */
+			$xml_IT->appendChild ($this->getResprocessingElement($dom, $item));
 			$dom->documentElement->appendChild ($xml_IT);
 		}
 		return $dom;
@@ -213,7 +158,130 @@ class EXP_Item_Ilias extends EXP_Item {
 	}
 	
 	
+	private function createResprocessingElementForChoice (DOMDocument $dom, EAL_Item $item): DOMElement {
+		
+		assert (($item instanceof EAL_ItemSC) || ($item instanceof EAL_ItemMC));
+		
+		$xml_RP = $dom->createElement("resprocessing");
+		
+		$xml_OC = $dom->createElement("outcomes");
+		$xml_DV = $dom->createElement("decvar");
+		$xml_OC->appendChild ($xml_DV);
+		$xml_RP->appendChild ($xml_OC);
+		
+		for ($index=0; $index<$item->getNumberOfAnswers(); $index++) {
+			foreach (array (1, 0) as $checked) {
+				
+				$xml_RC = $dom->createElement("respcondition");
+				$xml_RC->setAttribute ("continue", "Yes");
+				
+				$xml_CV = $dom->createElement("conditionvar");
+				$xml_NO = $dom->createElement("not");
+				$xml_VE = $dom->createElement("varequal", $index);
+				$xml_VE->setAttribute ("respident", $this->getQuestionIdent($item));
+				
+				if ($checked==1) {
+					$xml_CV->appendChild ($xml_VE);
+				} else {
+					$xml_NO->appendChild ($xml_VE);
+					$xml_CV->appendChild ($xml_NO);
+				}
+				$xml_RC->appendChild ($xml_CV);
+				
+				
+				if ($item instanceof EAL_ItemSC) {
+					$xml_SV = $dom->createElement("setvar", ($checked==1) ? $item->getPointsChecked($index) : 0);
+				}
+				if ($item instanceof EAL_ItemMC) {
+					$xml_SV = $dom->createElement("setvar", ($checked==1) ? $item->getPointsPos($index) : $item->getPointsNeg($index));
+				}
+				
+				$xml_SV->setAttribute ("action", "Add");
+				$xml_RC->appendChild ($xml_SV);
+				$xml_RP->appendChild ($xml_RC);
+				
+			}
+			
+		}
+		
+		return $xml_RP;
+	}
+	
+	
+	private function createResprocessingElementForText (DOMDocument $dom, EAL_ItemFT $item): DOMElement {
+		
+		$xml_RP = $dom->createElement('resprocessing');
+		$xml_RP->setAttribute('scoremodel', 'HumanRater');
+		
+		$xml_OC = $dom->createElement('outcomes');
+		$xml_DV = $dom->createElement('decvar');
+		$xml_DV->setAttribute('maxvalue', $item->getPoints());
+		$xml_DV->setAttribute('minvalue', '0');
+		$xml_DV->setAttribute('vartype', 'Integer');
+		$xml_DV->setAttribute('varname', 'WritingScore');
+		$xml_OC->appendChild ($xml_DV);
+		$xml_RP->appendChild ($xml_OC);
+		
+		
+		$xml_RC = $dom->createElement('respcondition');
+		$xml_CV = $dom->createElement('conditionvar');
+		$xml_OT = $dom->createElement('other', 'tutor_rated');
+		$xml_CV->appendChild($xml_OT);
+		$xml_RC->appendChild($xml_CV);
+		$xml_RP->appendChild ($xml_RC);
+		
+		return $xml_RP;
+		
+	}
+	
+	
+	
+	private function createResponseElementForChoice (DOMDocument $dom, EAL_Item $item, string $rcardinality): DOMElement {
+		
+		assert (($item instanceof EAL_ItemSC) || ($item instanceof EAL_ItemMC));
+		
+		$xml_RC = $dom->createElement("render_choice");
+		$xml_RC->setAttribute("shuffle", "Yes");
+		if ($item instanceof EAL_ItemMC) {
+			$xml_RC->setAttribute("minnumber", $item->minnumber);
+			$xml_RC->setAttribute("maxnumber", $item->maxnumber);
+		}
+		
+		for ($index=0; $index<$item->getNumberOfAnswers(); $index++) {
+			$xml_LAB = $dom->createElement("response_label");
+			$xml_LAB->setAttribute("ident", $index);
+			$xml_LAB->appendChild ($this->createMaterialElement($dom, "text/html", $item->getAnswer ($index)));
+			$xml_RC->appendChild ($xml_LAB);
+		}
 
+		$xml_RL = $dom->createElement("response_lid");
+		$xml_RL->setAttribute('ident', $this->getQuestionIdent($item));
+		$xml_RL->setAttribute('rcardinality', $rcardinality);
+		$xml_RL->appendChild ($xml_RC);
+		return $xml_RL;
+		
+	}
+	
+	
+	
+	private function createResponseElementForText (DOMDocument $dom, EAL_ItemFT $item): DOMElement {
+		
+		$xml_RF = $dom->createElement("render_fib");
+		$xml_RF->setAttribute ('prompt', 'Box');
+		$xml_RF->setAttribute ('fibtype', 'String');
+		
+		$xml_RL = $dom->createElement("response_label");
+ 		$xml_RL->setAttribute ('ident', 'A');
+ 		$xml_RF->appendChild ($xml_RL); 		
+		
+ 		$xml_RS = $dom->createElement("response_str");
+		$xml_RS->setAttribute('ident', 'TEXT');
+		$xml_RS->setAttribute('rcardinality', 'Ordered');
+		
+		$xml_RS->appendChild ($xml_RF);
+		return $xml_RS;
+	}
+	
 	
 	private function createMaterialElement ($dom, $type, $value) {
 		
